2d83d9359379e83b3baac8aa378c633e368e9691
akash7sridhar <akash7sridhar@gmail.com>
2022-03-14 07:50:00 +0530

    chore:replace aws command with variable

--- cloud/aws/local-exec/main.tf
+++ cloud/aws/local-exec/main.tf
@@ -1,6 +1,12 @@
+locals {
+  name            = "akash"
+  cluster_version = "1.21"
+  region          = "ap-south-1"
+}
+
 resource "null_resource" "config_kubectl" {
   provisioner "local-exec" {
-   command = "mkdir ${var.file}-${var.region}"
+   command = "mkdir ${local.name}-${local.region}"
 
   }
 
